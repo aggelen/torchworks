beba86b524129775eb416fbbdd910c9f977a2a8d
aggelen <aggelen@outlook.com>
2021-11-14 13:53:08 +0300

    train v1, structure fix
    move loss and optimizer to experiment class

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# torchworks
+### TorchWorks
+
+- Easily code and run pytorch-based experiments with your own models!
+- Torchworks is a framework for simplify training, validation, and testing processes with many networks and losses.
+- It was designed to reduce code mess and keep works as experiments.
+
+####Javascript　
+
+```python
+class Experiment:
+    def __init__(self, network, experiment_params):
+        self.network = network
+        
+        self.no_epoch = experiment_params['no_epoch']
+
+        #Optimizer
+        if experiment_params['optimizer'] == 'default':
+            self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-4, betas=(0.9,0.999))
+        else:
+            self.optimizer = experiment_params['optimizer']
+
+        self.loss = experiment_params['loss']
+        self.data_loader = experiment_params['data_loader']
+
+```
--- TorchWorks/Works.py
+++ TorchWorks/Works.py
@@ -4,29 +4,56 @@
 Created on Thu Nov 11 13:51:50 2021
 
 @author: Aykut Görkem Gelen
+@author: Eyyüp Yıldız
+
 """
+import torch
+from tqdm import tqdm
 
 class Experiment:
-    def __init__(self, network, hyper_params, experiment_params):
+    def __init__(self, network, experiment_params):
         self.network = network
-        self.hyper_params = hyper_params
-        self.experiment_params = experiment_params
         
+        self.no_epoch = experiment_params['no_epoch']
+
+        #Optimizer
+        if experiment_params['optimizer'] == 'default':
+            self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-4, betas=(0.9,0.999))
+        else:
+            self.optimizer = experiment_params['optimizer']
+
+        self.loss = experiment_params['loss']
+        self.data_loader = experiment_params['data_loader']
+
     def train(self):
-         #FIXME! dataloader
-        dataloder = []
- 
-        for k in range(self.experiment_params['no_epoch']):
-            
-            #FIXME! dataloader
-            for batch_data in dataloder:
-                forward_output = self.network(batch_data)
+        self.network.train()            #FIXME! is this really needed?
         
+        for k in range(self.no_epoch):
+            running_loss = 0.0
+            
+            for batch_id, batch_data in tqdm(enumerate(self.data_loader)):
+                
+                forward_data, target_data = batch_data
+                
+                self.optimizer.zero_grad()
+                
+                #forward + backward + optimize
+                forward_output = self.network(forward_data)
+                
+                loss = self.loss(forward_output, target_data)
+                loss.backward()
+                self.optimizer.step()
+                
+                running_loss += loss.item()
+                
+            # print statistics
+            print('RunningLoss: {} @ Epoch #{}'.format(running_loss / self.data_loder.__len__(), k+1))
+
         print('Train Completed!')
-    
+
     def test(self):
         self.network.eval()
         pass
-    
+
     def validation(self):
         pass
